37797828b0f6fee91db76a4029ae126e0f35896b
Levent Yumerov <yumerov.levent@gmail.com>
2017-11-19 12:13:26 +0200

    running laravel server after runnning container

---
+++ docker-build.sh
@@ -0,0 +1,1 @@
+docker build -t imgdb:v1 .
--- docker-run.sh
+++ docker-run.sh
@@ -1,3 +1,1 @@
-docker build -t imgdb:v1 .
-# docker run -d -p 8000:8000 --name imgdb-app imgdb
-docker run -p 8000:8000 -i -t imgdb:v1
+docker run -d -v $(pwd):/app -p 8000:8000 imgdb:v1
