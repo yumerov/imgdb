a477bebc729e7bd34bed48764f20f3fc8fd4fe51
Levent Yumerov <levent.yumerov@mentormate.com>
2017-12-21 14:59:46 +0200

    Tag factory adn seeder

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -22,3 +22,10 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+$factory->define(App\Tag::class, function (Faker\Generator $faker) {
+    return [
+        'name' => $faker->words(mt_rand(1, 3), true),
+    ];
+});
---
+++ database/seeds/TagSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Tag;
+
+class TagSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Tag::truncate();
+        factory(Tag::class, 10)->create()->each(function ($tag) {
+            $tag->save();
+        });
+    }
+}
